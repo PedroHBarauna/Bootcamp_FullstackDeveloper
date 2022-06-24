c2c649626fd8061b774bf77ec2c22f10866aaae3
PedroHBarauna <barauna38@gmail.com>
2022-06-24 07:45:29 -0300

    mandando o resto

--- curso_javascript/variaveis_e_tipos/verifyPalindromo.js
+++ curso_javascript/variaveis_e_tipos/verifyPalindromo.js
@@ -12,11 +12,11 @@ const verifyPalindromo2 = (string) => {
     if(!string) return "string inexistente";
 
     for(let i=0;i<string.length/2;i++){
-        
+        if(string[i] !== string[string.length-1-i]){
+            return false;
+        }
     }
-
-    return string === palindromoReverse;
-
+    return true;
 }
 
 
